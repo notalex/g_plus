e31549bc6c48a6bb55ce9cf08edfde92c7706215
Alex Johnson <notalexjohnson@gmail.com>
2014-04-25 10:33:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPlus::Application.config.secret_token = '32052e4a09c53e3c76df440acdf95104d175d9f229d6d632aa05a98e0c96e74692be325a78def6fc56c346854fc433ab4b261ce515ead3f8bde40f660273229f'
