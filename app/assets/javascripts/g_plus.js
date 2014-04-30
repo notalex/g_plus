13d1b1e48e78d1da8a3f777e4b44aa51bfd521b9
Alex Johnson <notalexjohnson@gmail.com>
2014-04-30 19:37:39 +0530

    Log user information after signin

--- app/assets/javascripts/g_plus.js
+++ app/assets/javascripts/g_plus.js
@@ -4,14 +4,12 @@ $(function() {
   })
 
   function signInCallback(authResult) {
-    if (authResult['status']['signed_in']) {
-      console.log(gapi.auth.getToken().access_token);
-    } else {
-      // Possible error values:
-      //   "user_signed_out" - User is signed-out
-      //   "access_denied" - User denied access to your app
-      //   "immediate_failed" - Could not automatically log in the user
-      console.log('Sign-in state: ' + authResult['error']);
-    }
+    gapi.client.load('plus','v1', function(){
+      var request = gapi.client.plus.people.get({'userId': 'me'});
+
+      request.execute(function(resp) {
+        console.log(resp);
+      });
+    });
   }
 })
