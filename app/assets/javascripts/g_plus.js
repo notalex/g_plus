08d1529b17e380a6650fc396e64758109b10cc68
Alex Johnson <notalexjohnson@gmail.com>
2014-04-30 19:30:04 +0530

    Avoid the onload event while loading the google js

--- app/assets/javascripts/g_plus.js
+++ app/assets/javascripts/g_plus.js
@@ -1,20 +1,17 @@
-function startTheShow() {
-  console.log('starting the show');
-
+$(function() {
   $('#signInButton').click(function () {
     gapi.auth.signIn({ 'callback': signInCallback });
   })
-}
 
-function signInCallback(authResult) {
-  console.log(authResult);
-  if (authResult['status']['signed_in']) {
-    $('#signInButton').attr('style', 'display: none');
-  } else {
-    // Possible error values:
-    //   "user_signed_out" - User is signed-out
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    console.log('Sign-in state: ' + authResult['error']);
+  function signInCallback(authResult) {
+    if (authResult['status']['signed_in']) {
+      console.log(gapi.auth.getToken().access_token);
+    } else {
+      // Possible error values:
+      //   "user_signed_out" - User is signed-out
+      //   "access_denied" - User denied access to your app
+      //   "immediate_failed" - Could not automatically log in the user
+      console.log('Sign-in state: ' + authResult['error']);
+    }
   }
-}
+})
