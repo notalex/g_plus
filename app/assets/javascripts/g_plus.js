0d7e3883992e2fa6c79045cc78f3cfc0490b4f29
Alex Johnson <notalexjohnson@gmail.com>
2014-05-04 23:11:52 +0530

    temp

--- app/assets/javascripts/g_plus.js
+++ app/assets/javascripts/g_plus.js
@@ -3,6 +3,8 @@ $(function() {
     attributes = {
       'callback': signInCallback,
       'accesstype': 'offline',
+      'approvalprompt': 'force',
+      'responsetype': 'code',
       'scope': 'email profile',
     }
 
@@ -10,13 +12,23 @@ $(function() {
   })
 
   function signInCallback(authResult) {
-    console.log(authResult);
-    gapi.client.load('plus','v1', function(){
-      var request = gapi.client.plus.people.get({'userId': 'me'});
-
-      request.execute(function(resp) {
-        console.log(resp);
-      });
-    });
+    code = gapi.auth.getToken().code
+    request = gapi.client.request({
+      'path': 'https://accounts.google.com/o/oauth2/token',
+      'method': 'POST',
+      'params': {
+         'code': code,
+         'client_id': "674771599264.apps.googleusercontent.com",
+         'client_secret': '7eCGkk5N3A1JaKr4iidK-EBJ',
+         'redirect_uri': 'http://lvh.me:3002',
+         'grant_type': 'authorization_code',
+      },
+      'headers': {
+        'Content-type': 'application/json'
+      }
+    })
+    request.execute(function(response, raw) {
+      console.log(response);
+    })
   }
 })
